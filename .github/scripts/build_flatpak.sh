7c9a2736a6cf0ec329602ed0ba48e756f5f2cd81
Suyog Tandel <git@suyogtandel.in>
2026-02-26 22:00:58 +0530

    fix(pkg): flatpak build script failing due to absence of zsh

--- .github/scripts/build_flatpak.sh
+++ .github/scripts/build_flatpak.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 set -e
 
 PLATFORM=$1
@@ -6,21 +6,21 @@ VERSION=${version:-"0.0.0"}
 
 if [[ "$PLATFORM" == "ubuntu-24.04-arm" ]]; then
     ARCH="arm64"
+    FLATPAK_ARCH="aarch64"
 else
     ARCH="x86-64"
+    FLATPAK_ARCH="x86_64"
 fi
 
-# Setup flathub repository
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/
+echo "Building Flatpak for architecture: $FLATPAK_ARCH (Release: $VERSION)"
 
-# Install flatpak runtime and SDKs
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 flatpak install --user -y flathub \
     org.freedesktop.Platform//25.08 \
     org.freedesktop.Sdk//25.08 \
     org.freedesktop.Sdk.Extension.rust-stable//25.08
 
-# Build the flatpak
-flatpak-builder --user --force-clean --repo=repo build-dir .github/manifests/in.suyogtandel.picoforge.json
+flatpak-builder --user --arch="${FLATPAK_ARCH}" --force-clean --repo=repo build-dir .github/manifests/in.suyogtandel.picoforge.json
 
-# Create the flatpak bundle
 flatpak build-bundle repo "picoforge_${VERSION}_${ARCH}.flatpak" in.suyogtandel.picoforge
