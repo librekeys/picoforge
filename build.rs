c0412aff74558356a08e725d30e6687fac90f9be
Fabrice Bellamy <38373466+Lab-8916100448256@users.noreply.github.com>
2026-02-28 22:45:17 +0100

    fix(ui): Application icon on macos and windows (#74)
    * change cargo-packager config to fix application icon on macos
    
    * fix: add windows icon fixes
    
    * chore(pkg): add cargo-packager removed stuff and clean build.rs
    
    * fix: compile issue on unix
    
    ---------
    
    Co-authored-by: Suyog Tandel <git@suyogtandel.in>

---
+++ build.rs
@@ -0,0 +1,13 @@
+#[cfg(windows)]
+use tauri_winres;
+
+// Configures windows application resource.( fix for app icon and launching app as admin)
+#[cfg(windows)]
+fn main() {
+    let mut res = tauri_winres::WindowsResource::new();
+    res.set_icon("static/appIcons/icon.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() {}
